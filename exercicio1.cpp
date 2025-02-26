54af735dd00193e788d5b44c33d0f35080ac23c5
samuelbatistadeb <139145373+samuelbatistadeb@users.noreply.github.com>
2025-02-26 13:48:55 -0300

    exercicio 1

---
+++ exercicio1.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    /*ENTRADA 
+    2 NUMEROS
+    */
+    float n1, n2;
+    
+    cout << "Digite o primeiro numero: ";
+    cin >> n1;
+
+    cout << "Digite o segundo numero: ";
+    cin >> n2;
+
+    /*PROCESSAMENTO 
+    SOMAR, DIMINUIR, MULTIPLICAR E DIVIDIR
+    soma, subtracao, mult, divi
+    */
+    float soma, subtracao, mult, divi;
+    soma = n1+n2;
+    subtracao = n1 - n2;
+    mult = n1 * n2;
+    divi = n1 / n2;
+
+    /* SAIDA
+    RESULTADO DAS OPERACOES*/
+    cout << "A soma dos numeros e: " << soma; 
+    cout << "A subtracao dos numeros e: " << subtracao; 
+    cout << "A multiplicacao dos numeros e: " << mult; 
+    cout << "A divisao dos numeros e: " << divi; 
+
+}
